d77a41ffe0a9c2f4e45a8614d1259994f1998164
LOGMPA <mpamarcelamarques@gmail.com>
2025-11-13 18:13:25 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/LOG/",
   plugins: [react()],
-  base: "./",
-});
+}));
